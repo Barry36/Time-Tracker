c44e8edf339f430007fde815bd2550cd933f581f
hoffman <hoffmanshf@outlook.com>
2020-06-26 23:12:05 -0400

    added MainActivity and EventListFragment to display event list in main page, implemented navigation bar

--- TimeTracker/app/src/main/java/com/cs446/group18/timetracker/adapter/EventListAdapter.java
+++ TimeTracker/app/src/main/java/com/cs446/group18/timetracker/adapter/EventListAdapter.java
@@ -1,35 +1,40 @@
 package com.cs446.group18.timetracker.adapter;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cs446.group18.timetracker.R;
+import com.cs446.group18.timetracker.databinding.ListItemEventBinding;
 import com.cs446.group18.timetracker.entity.Event;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder> {
-    private List<Event> events = new ArrayList<>();
+    private List<Event> events;
+    private LayoutInflater layoutInflater;
+
+    public EventListAdapter(List<Event> events) {
+        this.events = events;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()) // get context from main Activity
-                .inflate(R.layout.entry_item, parent, false);
-        return new ViewHolder(itemView);
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.getContext()); // get context from main Activity
+        }
+        ListItemEventBinding binding = DataBindingUtil.inflate(layoutInflater, R.layout.list_item_event, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Event currentEvent = events.get(position);
-        holder.textViewTitle.setText(currentEvent.getEventName());
-        holder.textViewDescription.setText(currentEvent.getDescription());
+        holder.bind(currentEvent);
     }
 
     @Override
@@ -39,18 +44,24 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
 
     // get List of LiveData
     public void setEvents(List<Event> events) {
-        this.events = events;
+        this.events.clear();
+
+        this.events.addAll(events);
         // there's more efficient way to update adapter
         notifyDataSetChanged();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView textViewTitle;
-        private TextView textViewDescription;
-        public ViewHolder(View itemView) {
-            super(itemView);
-            textViewTitle = itemView.findViewById(R.id.text_view_title);
-            textViewDescription = itemView.findViewById(R.id.text_view_description);
+        private ListItemEventBinding binding;
+
+        ViewHolder(@NonNull ListItemEventBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        void bind(Event event) {
+            binding.setEvent(event);
+            binding.executePendingBindings();
         }
     }
 
