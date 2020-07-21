ec37f5c6c5b1eb377fde15a7e20347db4f2849e1
hoffman <hoffmanshf@outlook.com>
2020-07-21 16:49:26 -0400

    removed project entity

--- TimeTracker/app/src/main/java/com/cs446/group18/timetracker/persistence/TimeTrackerDatabase.java
+++ TimeTracker/app/src/main/java/com/cs446/group18/timetracker/persistence/TimeTrackerDatabase.java
@@ -13,17 +13,15 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 import com.cs446.group18.timetracker.dao.EventDao;
 import com.cs446.group18.timetracker.dao.GeolocationDao;
 import com.cs446.group18.timetracker.dao.GoalDao;
-import com.cs446.group18.timetracker.dao.ProjectDao;
 import com.cs446.group18.timetracker.dao.TimeEntryDao;
 import com.cs446.group18.timetracker.entity.Event;
 import com.cs446.group18.timetracker.entity.Geolocation;
 import com.cs446.group18.timetracker.entity.Goal;
-import com.cs446.group18.timetracker.entity.Project;
 import com.cs446.group18.timetracker.entity.Tag;
 import com.cs446.group18.timetracker.entity.TimeEntry;
 import com.cs446.group18.timetracker.utils.DateTimeConverter;
 
-@Database(entities = {TimeEntry.class, Event.class, Tag.class, Project.class, Goal.class, Geolocation.class}, version = 1, exportSchema = false)
+@Database(entities = {TimeEntry.class, Event.class, Tag.class, Goal.class, Geolocation.class}, version = 1, exportSchema = false)
 @TypeConverters({DateTimeConverter.class})
 public abstract class TimeTrackerDatabase extends RoomDatabase {
     // create a singleton instance of database
@@ -32,8 +30,6 @@ public abstract class TimeTrackerDatabase extends RoomDatabase {
     public abstract TimeEntryDao timeEntryDao();
 
     public abstract EventDao eventDao();
-
-    public abstract ProjectDao projectDao();
 
     public abstract GoalDao goalDao();
 
@@ -63,30 +59,25 @@ public abstract class TimeTrackerDatabase extends RoomDatabase {
     private static class PopulateDBAsyncTask extends AsyncTask<Void, Void, Void> {
         private TimeEntryDao timeEntryDao;
         private EventDao eventDao;
-        private ProjectDao projectDao;
         private GoalDao goalDao;
         private GeolocationDao geolocationDao;
 
         private PopulateDBAsyncTask(TimeTrackerDatabase db) {
             this.timeEntryDao = db.timeEntryDao();
             this.eventDao = db.eventDao();
-            this.projectDao = db.projectDao();
             this.goalDao = db.goalDao();
             this.geolocationDao = db.geolocationDao();
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
-            projectDao.insert(new Project("Study", "N/A", "#FF4081"));
-            projectDao.insert(new Project("Rest", "N/A", "#3F51B5"));
-            eventDao.insert(new Event(1, "Study", "LC 161 & LC 162"));
-            eventDao.insert(new Event(2, "Rest", "Watch drama"));
+            eventDao.insert(new Event("Study", "LC 161 & LC 162"));
+            eventDao.insert(new Event("Rest", "Watch drama"));
             timeEntryDao.insert(new TimeEntry(1, DateTimeConverter.fromTimestamp("2020-07-22 18:20:00"), DateTimeConverter.fromTimestamp("2020-07-22 18:40:00"), 1200L));
             timeEntryDao.insert(new TimeEntry(1, DateTimeConverter.fromTimestamp("2020-07-23 10:15:00"), DateTimeConverter.fromTimestamp("2020-07-23 10:20:00"), 300L));
             timeEntryDao.insert(new TimeEntry(2, DateTimeConverter.fromTimestamp("2020-07-22 11:15:00"), DateTimeConverter.fromTimestamp("2020-07-22 11:40:00"), 1500L));
             timeEntryDao.insert(new TimeEntry(2, DateTimeConverter.fromTimestamp("2020-07-18 11:15:00"), DateTimeConverter.fromTimestamp("2020-07-18 11:40:00"), 1500L));
             timeEntryDao.insert(new TimeEntry(2, DateTimeConverter.fromTimestamp("2020-07-17 11:15:00"), DateTimeConverter.fromTimestamp("2020-07-17 11:40:00"), 1500L));
-
             goalDao.insert(new Goal(1, "Study Goal", "I have to study for my final exam", 20, 100));
             goalDao.insert(new Goal(2, "Rest Goal", "I need some rest", 80, 100));
             geolocationDao.insert(new Geolocation(1, 43.46552, -80.5226817)); // Captain Boil
